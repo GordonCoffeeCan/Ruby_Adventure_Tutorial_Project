bf9c035ee9e20836bc1cbb6180cf923ac4d2ffa5
gordonlee <gordon.lee.6170@gmail.com>
2020-12-11 15:04:42 -0800

    start for the UI

---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public Image mask;
+    float originalSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //设置当前UI血条显示值
+    public void SetValue(float fillPercent) {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * fillPercent);
+    }
+}
